3e1bf453e9a5bc23ab86280b7ef5154d3623ee46
Prak94 <61499308+Prak94@users.noreply.github.com>
2020-05-27 18:31:05 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,7 +1,7 @@
 public class AddTwoNumbers {
 
    public static void main(String[] args) {
-        
+      //sum example  
       int num1 = 5, num2 = 15, sum;
       sum = num1 + num2;
       System.out.println("Number 1: "+num1);
